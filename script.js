ba5110ade3861dd25f984b9669eabd749be860ce
Shubham-rawal375 <rawal3983@gmail.com>
2026-07-03 13:08:01 +0530

    Create script.js

---
+++ script.js
@@ -0,0 +1,5 @@
+// Map Critical Operational Selectors
+const ledgerForm = document.getElementById('ledger-form');
+const textDescription = document.getElementById('text-description');
+const numericalAmount = document.getElementById('numerical-amount');
+const transactionList = document.getElementById('transaction
